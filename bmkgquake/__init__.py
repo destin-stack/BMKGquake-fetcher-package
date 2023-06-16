5f97ef5d50395bb9a004097c58baf2e2289cdebe
destinerikanb <destinerikanb@gmail.com>
2023-06-17 00:36:58 +0700

    adding modules in init so it can be used when importing the package

--- bmkgquake/__init__.py
+++ bmkgquake/__init__.py
@@ -1,6 +1,8 @@
-import extract_data
-import display_data
+from bmkgquake.display_data import display_data
+from bmkgquake.extract_data import extract_data
+
+__all__ = ['display_data', 'extract_data']
 
 if __name__ == '__main__':
-    result = extract_data.extract_data()
-    display_data.display_data(result)
+    result = extract_data()
+    display_data(result)
